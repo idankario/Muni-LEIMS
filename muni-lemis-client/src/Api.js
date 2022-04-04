f1a79cccc295ba69ddffa7b0c532fc2710b0b5eb
Mhmdxx5 <48560391+mhmdxx5@users.noreply.git>
2022-04-04 21:20:10 +0300

    slice

--- muni-lemis-client/src/Api.js
+++ muni-lemis-client/src/Api.js
@@ -3,11 +3,12 @@ const PROTOCOL='http'
 const PORT=3000
 const API_URL = `${PROTOCOL}://${HOST}:${PORT}`
 
-export async function uploadImage(file) {
+export async function uploadImage(file, area) {
     var data = new FormData()
     data.append('file', file)
+    data.append('area', area)
     let res = await fetch(`${API_URL}/imgupload/upload`, {method: 'POST', body: data})
-    res = await res.json()
+    // res = await res.json()
     return res
 }
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const usersCtl = require('./controllers/users.js');
@@ -10,10 +11,9 @@ const boxesCtl = require('./controllers/boundingbox.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 // in latest body-parser use like below.
 app.use(bodyParser.urlencoded({ extended: true }));
 
